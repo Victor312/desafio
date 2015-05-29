87585ba850fedff22e0701973f4f95efe7db6e2a
Victor312 <andrevftorres@gmail.com>
2015-05-29 15:12:47 -0300

    Commit Inicial

---
+++ _php/compra.php
@@ -0,0 +1,58 @@
+<html>
+	<head>
+		<meta charset = "UTF-8"/>
+		<title>ESTAMPMAX</title>
+		<link rel = "stylesheet" type = "text/css" href = "TCC.css"/>
+		<link rel="stylesheet" type="text/css" href="slide_css.css"/>
+		<script type="text/javascript" src="slide_script.js"></script>
+	</head>
+	<body>
+		<div id ="wrapper"><!--wapper-->
+		
+			<div id = "header"><!--header-->
+			A
+			</div><!--/header-->
+			
+			<div id = "nav"><!--nav-->
+				<ul class = "title">
+					<li class = "title"><a href ="DEFINI.html"> HOME</a></li>
+					<li class = "title"><a href= "camisetas.html">CAMISETAS</a></li>
+					<li class = "title"><a href= "acessorios.html">ACESSORIOS </a></li>
+					<li class = "title"><a href = "contato.html">CONTATO</a></li>
+				</ul>
+			</div><!--/nav-->
+	
+			<div id = "content2"><!--content2-->
+				
+				<?php
+				include "form_insere.php";
+				
+							
+				?>
+	
+			</div><!--/content2-->
+	
+			
+			<div id = "sidebar2"><!--sidebar--> 
+				<ul>
+					<img src = "1.jpg" class= "imagens"/>
+					<img src = "2.jpg" class= "imagens"/>
+					<img src = "3.jpg" class= "imagens"/>
+					<img src = "4.jpg" class= "imagens"/>
+					<img src = "5.jpg" class= "imagens"/>
+					<img src = "6.jpg" class= "imagens"/>
+					
+				</ul>
+				
+				
+			</div><!--/sidebar2-->
+	
+
+	
+			<div id = "footer"><!--/footer-->
+				<p class= "direito">Direitos autorais de Andre Victor Fernandes Torres</p>
+			</div><!--/footer-->
+
+		</div><!-- / wrapper -->
+	</body>
+</html>
